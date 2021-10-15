c640c661fb74cd2fe06ae28b84f424f17e581582
Kate Storrs <katherine.storrs@gmail.com>
2021-10-15 15:01:48 +0200

    set new net training

--- wrapper_scripts/predrnn_v2_reflectionworld_train.py
+++ wrapper_scripts/predrnn_v2_reflectionworld_train.py
@@ -8,32 +8,32 @@ call(['python', '../run_predrnn.py',\
 '--dataset_name', 'reflectionworld',\
 '--train_data_paths', '../../data/generator6v3_10k/categorised',\
 '--valid_data_paths', '../../data/generator6v3_10k/categorised',\
-'--save_dir', '../../outputs_and_trained_networks/generator6v3_10k_20211014',\
-'--gen_frm_dir', '../../outputs_and_trained_networks/generator6v3_10k_20211014',\
+'--save_dir', '../../outputs_and_trained_networks/generator6v3_10k_20211015_128',\
+'--gen_frm_dir', '../../outputs_and_trained_networks/generator6v3_10k_20211015_128',\
 '--input_length', '10',\
 '--total_length', '20',\
 '--img_width', '128',\
 '--img_channel', '1',\
 '--model_name', 'predrnn_memory_decoupling',\
 '--pretrained_model', '',\
-'--num_hidden', '64,64,64,64',\
+'--num_hidden', '128,128,128,128',\
 '--filter_size', '5',\
 '--stride', '1',\
 '--patch_size', '4',\
 '--layer_norm', '0',\
 '--decouple_beta', '0.01',\
-'--reverse_scheduled_sampling', '0',\
-'--r_sampling_step_1', '2500',\
-'--r_sampling_step_2', '25000',\
+'--reverse_scheduled_sampling', '1',\
+'--r_sampling_step_1', '5000',\
+'--r_sampling_step_2', '50000',\
 '--r_exp_alpha', '2000',\
-'--scheduled_sampling', '1',\
-'--sampling_stop_iter', '30000',\
-'--sampling_start_value', '1.0',\
-'--sampling_changing_rate', '0.00002',\
+# '--scheduled_sampling', '1',\
+# '--sampling_stop_iter', '40000',\
+# '--sampling_start_value', '1.0',\
+# '--sampling_changing_rate', '0.00002',\
 '--lr', '0.0001',\
 '--reverse_input', '1',\
 '--batch_size', '8',\
-'--max_iterations', '40001',\
+'--max_iterations', '120001',\
 '--display_interval', '10',\
 '--test_interval', '5000',\
 '--snapshot_interval', '5000',\
